b6d9434223205e3622b21beaab364cc1395bdeb4
Haijin Yan <life0fun@gmail.com>
2013-08-13 20:52:52 -0700

    remove stale directories

--- project.clj
+++
@@ -1,3 +0,0 @@
-(defproject storm-logstream "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
--- test/storm_threshold/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns storm-threshold.test.core
-  (:use [storm-threshold.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
